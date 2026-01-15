2b1784cbad21f974017d463ff8dac8f474dfbb33
wikilixs <wikilixs12@gmail.com>
2026-01-15 14:53:17 -0400

    Agregar archivos de proyecto.

---
+++ RRHH_consola/RRHH.Entidades/Carga.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RRHH_consola.RRHH.Entidades
+{
+    internal class Carga
+    {
+    }
+}
---
+++ RRHH_consola/RRHH.Estenciones/Extenciones.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RRHH_consola.RRHH.Estenciones
+{
+    internal class Extenciones
+    {
+    }
+}
